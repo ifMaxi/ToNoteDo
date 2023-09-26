162e7f69d5ed091eb86053e4bb9d60b3a7b6919e
Maxi Pelizzoni <maxi96p@gmail.com>
2023-09-26 15:16:49 -0300

    Updated database class

--- app/src/main/java/com/maxidev/grocerylist/data/db/AppDataBase.kt
+++ app/src/main/java/com/maxidev/grocerylist/data/db/AppDataBase.kt
@@ -3,9 +3,16 @@ package com.maxidev.grocerylist.data.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.maxidev.grocerylist.data.db.dao.GroceryDao
+import com.maxidev.grocerylist.data.db.dao.RecipeDao
 import com.maxidev.grocerylist.data.db.entities.GroceryEntity
+import com.maxidev.grocerylist.data.db.entities.RecipeEntity
 
-@Database(entities = [GroceryEntity::class], version = 1, exportSchema = false)
+@Database(
+    entities = [GroceryEntity::class, RecipeEntity::class],
+    version = 1,
+    exportSchema = false
+)
 abstract class AppDataBase: RoomDatabase() {
-    abstract fun appDao(): GroceryDao
+    abstract fun groceryDao(): GroceryDao
+    abstract fun recipeDao(): RecipeDao
 }
