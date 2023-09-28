cd88e908e29450c785d28ae9dfaa90130cbc2800
Maxi Pelizzoni <maxi96p@gmail.com>
2023-09-28 14:34:48 -0300

    TypeConverters for image

--- app/src/main/java/com/maxidev/grocerylist/data/db/AppDataBase.kt
+++ app/src/main/java/com/maxidev/grocerylist/data/db/AppDataBase.kt
@@ -2,16 +2,19 @@ package com.maxidev.grocerylist.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.maxidev.grocerylist.data.db.dao.GroceryDao
 import com.maxidev.grocerylist.data.db.dao.RecipeDao
 import com.maxidev.grocerylist.data.db.entities.GroceryEntity
 import com.maxidev.grocerylist.data.db.entities.RecipeEntity
+import com.maxidev.grocerylist.utils.Converters
 
 @Database(
     entities = [GroceryEntity::class, RecipeEntity::class],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
+@TypeConverters(Converters::class)
 abstract class AppDataBase: RoomDatabase() {
     abstract fun groceryDao(): GroceryDao
     abstract fun recipeDao(): RecipeDao
--- app/src/main/java/com/maxidev/grocerylist/data/db/entities/RecipeEntity.kt
+++ app/src/main/java/com/maxidev/grocerylist/data/db/entities/RecipeEntity.kt
@@ -1,5 +1,6 @@
 package com.maxidev.grocerylist.data.db.entities
 
+import android.graphics.Bitmap
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
@@ -10,6 +11,6 @@ data class RecipeEntity(
     val id: Long,
     val recipeTitle: String,
     val recipeBody: String,
-    @ColumnInfo(name = "image",typeAffinity = ColumnInfo.BLOB)
-    val image: Byte?
+    @ColumnInfo(name = "image", typeAffinity = ColumnInfo.BLOB)
+    val image: Bitmap?
 )
