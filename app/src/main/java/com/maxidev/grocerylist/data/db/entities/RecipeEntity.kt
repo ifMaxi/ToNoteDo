196e4715c388b1828af95f7ef6fa15d97bf267a6
Maxi Pelizzoni <maxi96p@gmail.com>
2023-10-10 20:09:04 -0300

    Image parameter temporarily removed

--- app/src/main/java/com/maxidev/grocerylist/data/db/entities/RecipeEntity.kt
+++ app/src/main/java/com/maxidev/grocerylist/data/db/entities/RecipeEntity.kt
@@ -1,16 +1,12 @@
 package com.maxidev.grocerylist.data.db.entities
 
-import android.graphics.Bitmap
-import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "recipe")
 data class RecipeEntity(
     @PrimaryKey(autoGenerate = true)
-    val id: Long?,
+    val id: Long,
     val recipeTitle: String,
-    val recipeBody: String,
-    @ColumnInfo(name = "image", typeAffinity = ColumnInfo.BLOB)
-    val image: Bitmap?
+    val recipeBody: String
 )
